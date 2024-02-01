bd0b3b4d96ededc5b9b9e29761193298996e90db
SHERIDAN/srseid <seid@sheridancollege.ca>
2024-02-01 15:51:20 -0500

    fixing spawning plane

--- Assets/another.cs
+++ Assets/another.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour
+public class another : MonoBehaviour
 {
     public GameObject planes;
     float time;
     float speed = 5f;
-
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(planes);
-        
     }
 
     // Update is called once per frame
