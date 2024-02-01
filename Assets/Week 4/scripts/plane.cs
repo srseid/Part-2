3984b0036a7dfe2fb2dacf6c6b498322b3d8b2cb
SHERIDAN/srseid <seid@sheridancollege.ca>
2024-02-01 14:25:00 -0500

    moving the plane, by updating the flightpath

--- Assets/Week 4/scripts/plane.cs
+++ Assets/Week 4/scripts/plane.cs
@@ -8,11 +8,45 @@ public class plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Rigidbody2D rigidbody;
+    Vector2 currentPosition;
+    public float speed = 1;
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //physics
+        currentPosition = transform.position;
+        if(points.Count >0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+                //remove lines after followed
+                for(int i = 0; i<lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()
