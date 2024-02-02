969201fba51c74fa1649fc0c90bbba52c7456503
SHERIDAN/srseid <seid@sheridancollege.ca>
2024-02-02 12:12:59 -0500

    spawn other planes

--- Assets/another.cs
+++ Assets/another.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 public class another : MonoBehaviour
 {
     public GameObject planes;
-    float time;
-    float speed = 5f;
+    public Sprite[] sprites;
+    float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(planes);
+        
+        timer = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (time <= 0)
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
         {
-            Instantiate(planes);
-            time = Random.Range(50, 70);
+            timer = Random.Range(1,5);
+
+            GameObject plane = Instantiate(planes);
+            plane.transform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
+            plane.transform.eulerAngles = new Vector3(0, 0, Random.Range(0, 360f));
+
+            plane.GetComponent<plane>().speed = Random.Range(1f, 3f);
+            plane.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
         }
-        else
-        {
-            time = Time.deltaTime;
-        }
+       
     }
 }
